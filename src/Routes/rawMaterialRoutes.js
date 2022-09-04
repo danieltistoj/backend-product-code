287832fa7b8b70370eef2a8e35bb51185ef88410
danieltistoj <jose.tisrey@gmail.com>
2022-09-04 17:53:46 -0600

    crud de materia prima listo

--- src/Routes/rawMaterialRoutes.js
+++ src/Routes/rawMaterialRoutes.js
@@ -3,9 +3,17 @@ const router = express.Router();
 
 import {
     createRawMaterial,
+    getAllRawMaterial,
+    getOneRawMaterial,
+    updateRawMaterial,
+    deleteRawMaterial,
 } from "../controller/rawMaterialController.js"
 
 router
     .post("/createRawMaterial", createRawMaterial)
+    .get("/getAllRawMaterial",getAllRawMaterial)
+    .get("/getOneRawMaterial/:name",getOneRawMaterial)
+    .put("/upDateRawMaterial/:name",updateRawMaterial)
+    .delete("/deleteRawMaterial/:name",deleteRawMaterial)
 
 export default router
--- src/controller/rawMaterialController.js
+++ src/controller/rawMaterialController.js
@@ -6,7 +6,38 @@ const createRawMaterial = async (req,res)=>{
     res.send( await newCRUD.createService("Raw Material") )
 }
 
+const getAllRawMaterial =  async (req, res) =>{
+    const newCRUD = new crudService(req.body,RawMaterial)
+    res.send(await newCRUD.getAllData())
+}
+const getOneRawMaterial = async (req, res) =>{
+    const newCRUD = new crudService(req.body,RawMaterial)
+    res.send(await newCRUD.getOneData(req.params))
+}
+
+const updateRawMaterial = async (req, res) =>{
+    const newCRUD = new crudService(req.body,RawMaterial)
+    if(await newCRUD.validation(req.params)){
+        res.send(await newCRUD.updateService(req.params,req.body))
+    }else{
+        res.send("No data found")
+    }
+}
+
+const deleteRawMaterial = async (req,res)=>{
+    const newCRUD = new crudService(req.body,RawMaterial)
+    if(await newCRUD.validation(req.params)){
+        res.send(await newCRUD.deleteService(req.params))
+    }else{
+        res.send("No data found")
+    }
+}
+
 export {
     createRawMaterial,
+    getAllRawMaterial,
+    getOneRawMaterial,
+    updateRawMaterial,
+    deleteRawMaterial,
 }
 
