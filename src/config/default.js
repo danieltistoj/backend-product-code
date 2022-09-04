4d695cc7e793c0ed1658e0f249fa1b444a1f30ca
danieltistoj <jose.tisrey@gmail.com>
2022-09-04 00:59:33 -0600

    prueba a heroku

--- src/config/default.js
+++ src/config/default.js
@@ -4,9 +4,9 @@ dotenv.config()
 
 export const config = {
     api:{
-        port: process.env.PORT,
-        hostname: process.env.HOSTNAME,
-        name: process.env.NAME
+        port: process.env.PORT || 4000,
+        hostname: process.env.HOSTNAME || localhost,
+        name: process.env.NAME || App-product
     },
     uriMongo:{
         uri: process.env.MONGO_URI,
