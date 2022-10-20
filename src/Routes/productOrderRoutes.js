459a618953e1fd20969d9a6481a86ceadcd4727a
danieltistoj <jose.tisrey@gmail.com>
2022-10-19 23:28:08 -0600

    inicio crear orden producto

--- src/Routes/productOrderRoutes.js
+++ src/Routes/productOrderRoutes.js
@@ -11,12 +11,22 @@ export class ProductOrderRouter{
         
         this.router
                 .get("/",this.handleTest.bind(this))
+                .post("/createProductOrder",this.handleCreateProductOrder.bind(this))
                 
                 
     }
     async handleTest(req,res){
         try {
             const message =  "Test Product Order"
+            this.response.success(req,res,message,this.httpCode.CREATED)
+        } catch (error) {
+            this.response.error(req,res,error,this.httpCode.BAD_REQUEST)
+        }
+        
+    }
+    async handleCreateProductOrder(req,res){
+        try {
+            const message =  "Create Product Order"
             this.response.success(req,res,message,this.httpCode.CREATED)
         } catch (error) {
             this.response.error(req,res,error,this.httpCode.BAD_REQUEST)
