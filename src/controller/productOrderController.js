effa9fe8d607fbee772fd233ebd073966fff70b3
danieltistoj <jose.tisrey@gmail.com>
2022-10-21 19:18:55 -0600

    orden que recibe el pedido de otro dia

--- src/controller/productOrderController.js
+++ src/controller/productOrderController.js
@@ -9,30 +9,50 @@ export class ProductOrderController extends crudController{
         if(!await this.validation({idProduct:data.idProduct})){
            //obtenemos el producto 
             const product = await productController.getOne({_id:data.idProduct})
-            data.stock = product.stocks
-            data.total = data.stock 
-            data.newStock = data.total - data.sale
-            /*
-            si no existe una orden relacionada con el producto
-            lo que recibe es cero
-            */
-            data.arrival = 0
-            const newData ={
-                arrival: data.arrival,
-                stock: data.stock,
-                total: data.total,
-                order: data.order,
-                sale: data.sale,
-                newStock: data.newStock,
-                date: this.createDateFormat(date),
-                idProduct: data.idProduct,
-            }
-            
+            const newData = this.createOrder(data,0,product.stocks)
             return await this.create(newData)
         }else{
-            return "no existe la orden"
+             //obtenemos el producto 
+            const product = await productController.getOne({_id:data.idProduct})
+            //obtener todas las ordenes
+            const listOrder = await this.getAll()
+            //filtrar por el id del producto 
+            const filteredList = listOrder.filter(order => order.idProduct === data.idProduct)
+            //se obtienen los dias para entregar un producto y se retorna la fecha
+            const dateOrder = this.orderDate(date,product.deliveryQuantity)
+            console.log(dateOrder)
+            //se  obtiene el registro donde esta la orden
+            const desiredOrder = filteredList.find(order => order.date ===dateOrder)
+            if(!(desiredOrder === undefined)){
+                const order = desiredOrder.order
+                const stock = desiredOrder.newStock
+                const newData = this.createOrder(data,order,stock)
+                return await this.create(newData)
+            }else{
+                throw "there is no order to receive"
+            }
         }
-      
+    }
+    //formar orden
+    createOrder(data,orderOld,stockOld){
+        const date = new Date()
+        data.arrival = orderOld
+        data.stock = stockOld
+        data.total = data.arrival + data.stock
+        data.newStock = data.total - data.sale
+        data.date = this.createDateFormat(date)
+        const newData = {
+            arrival: data.arrival,
+            stock: data.stock,
+            total: data.total,
+            order: data.order,
+            sale: data.sale,
+            newStock: data.newStock,
+            date:data.date,
+            idProduct: data.idProduct
+        }
+        return newData
+
     }
     //obtenemos la fecha en la que se pidio una orden
     orderDate(date,days){
@@ -40,11 +60,11 @@ export class ProductOrderController extends crudController{
         let day = date.getDate()
         let month = date.getMonth() + 1
         let year = date.getFullYear()
-        return date 
+        return `${day}/${month}/${year}`
     }
     createDateFormat(data){
         const day = data.getDate()
-        const month = data.getMonth()
+        const month = data.getMonth() + 1
         const year = data.getFullYear()
         return `${day}/${month}/${year}`
     }
